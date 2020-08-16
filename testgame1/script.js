da4edee2b1fed99668a544e2b8327bda411283d7
Lavstrasni <69418952+Lavstrasni@users.noreply.github.com>
2020-08-16 13:10:43 +0200

    Add files via upload
    idk

---
+++ testgame1/script.js
@@ -0,0 +1,12 @@
+
+
+var text = "welcome to lav's game"
+
+document.getElementById('aText').innerHTML = text;
+
+
+
+
+var age = prompt('enter your username')
+
+document.getElementById('bText').innerHTML = age;

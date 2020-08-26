d2ececfe00cfa5acf4a4919a584ae7790ff0979c
Lavstrasni <69418952+Lavstrasni@users.noreply.github.com>
2020-08-26 14:38:07 +0100

    Add files via upload
    Added a better title, start game button (that doesnt do anything at the moment)

---
+++ Testgame1(new)/script.js
@@ -0,0 +1,16 @@
+
+
+/*
+
+var text = "welcome to lav's game"
+
+document.getElementById('aText').innerHTML = text;
+
+*/
+
+
+var age = prompt('enter your username')
+
+document.getElementById('bText').innerHTML = age;
+
+
